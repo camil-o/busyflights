9bed6c01e7f3749d1d6a3c5a1a1512d847a20079
Camilo Silva <camilo.s@gmail.com>
2017-07-20 09:51:24 -0500

    Adding sort logic

--- src/main/java/com/travix/medusa/busyflights/controller/SearchController.java
+++ src/main/java/com/travix/medusa/busyflights/controller/SearchController.java
@@ -5,8 +5,11 @@ import com.travix.medusa.busyflights.domain.busyflights.BusyFlightsResponse;
 import com.travix.medusa.busyflights.service.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.annotation.OrderUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -20,8 +23,18 @@ public class SearchController {
     private SearchService<BusyFlightsRequest, BusyFlightsResponse> service;
 
     @RequestMapping(value = "/search", method = RequestMethod.POST)
-    public @ResponseBody List<BusyFlightsResponse> search(
-            @RequestBody BusyFlightsRequest request) {
-        return service.search(request);
+    public @ResponseBody
+    List<BusyFlightsResponse> search(
+            @RequestBody BusyFlightsRequest request,
+            @RequestParam(value = "sortBy", defaultValue = "fare-asc") String sortBy) {
+
+        List<BusyFlightsResponse> response = service.search(request);
+        Collections.sort(response, getComparator(sortBy));
+        return response;
+    }
+
+    private Comparator<BusyFlightsResponse> getComparator(String sortBy) {
+        Comparator<BusyFlightsResponse> fareCompartor = Comparator.comparingDouble(b -> b.getFare());
+        return sortBy.endsWith("desc") ? fareCompartor.reversed() : fareCompartor;
     }
 }
